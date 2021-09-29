c3742bf2b6b6f660f73a4eb294e053f3f224fa6b
Gyeongho Jeon <70951270+Jeongyeongho93@users.noreply.github.com>
2021-09-30 00:31:30 +0900

    Create weather_city_weather_temperature.js

---
+++ weather_city_weather_temperature.js
@@ -0,0 +1,19 @@
+const API_KEY = "4a739ca02a1bb282c09e23df8775c507"
+
+function onGeoOK(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main}`/`${data.main.temp}`
+    });
+}
+function onGeoError(){
+    alert("No weather")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
+
